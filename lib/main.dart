53b9e8f526c497b499a40ee62f12bfa5ca875af9
danhausen <danihausen@live.com>
2021-03-05 21:00:37 -0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Eu respondo"),
+        ),
       ),
-    );
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballRandomNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
